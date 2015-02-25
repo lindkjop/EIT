c69f29520c8b809e73eba49abbfe602ee1e22194
lindkjop <pialindkjolen@gmail.com>
2015-02-25 10:12:58 +0100

    cleanup of index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 var server = require("./server");
-var router = require("./router");
-//var requestHandlers = require("./requestHandlers");
-/*
-var handle = {};
-handle["/"] = requestHandlers.main;
-handle["/books"] = requestHandlers.books;
-handle["/magazine"] = requestHandlers.magazine;
-handle["/about"] = requestHandlers.about;
-handle["/favorites"] = requestHandlers.favorites;
-handle["/profile"] = requestHandlers.profile;
-
-//server.start(router.route, handle);
