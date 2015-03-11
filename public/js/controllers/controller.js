3e84f1342ec4d6caa25f122fe3776f9156f1e6bd
lindkjop <pialindkjolen@gmail.com>
2015-03-11 09:37:22 +0100

    delete unnecessary file

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -1,15 +1,6 @@
 //This is not finished yet
 
-var app = angular.module('leseDyrApp', []);
-/** * Controls the Blog */ 
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
- console.log("Blog Controller reporting for duty."); }); 
-/** * Controls all other Pages */ 
+//var app = angular.module('leseDyrApp', []);
 
-app.controller('PageCtrl', function (/* $scope, $location, $http */) { 
-	console.log("Page Controller reporting for duty."); 
-// Activates the Carousel 
-$('.carousel').carousel({ interval: 5000 }); 
-// Activates Tooltips for Social Links 
-$('.tooltip-social').tooltip({ selector: "a[data-toggle=tooltip]" }) });
-</pre>`</div>
+/** Bookcontroller **/
+
